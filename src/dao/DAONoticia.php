012ffa80c9929d50b51d0c33ba4574c41660af56
guilhermebussidias <guilherme17.dias@gmail.com>
2016-05-05 19:53:04 +0200

    Insert y Update DAONoticia

--- src/dao/DAONoticia.php
+++ src/dao/DAONoticia.php
@@ -25,12 +25,27 @@ class DAONoticia {
       return null;
     }
   }
+  //Buscar noticia por contenido o titulo
+  function getNoticiaContenido($contenido){
+    try {
+      $sql = "SELECT * FROM Noticia WHERE titulo LIKE '%:tituloNoticia%' OR contenido LIKE '%:contenidoNoticia'";
+      $stmt = $this->conn->prepare($sql);
+      $stmt->execute(["tituloNoticia" => $contenido, "contenidoNoticia" => $contenido]);
+      $res = $stmt->fetchAll();
+    } catch(PDOException $e) {
+  		echo "ERROR EN DAONoticia: " . $e->getMessage();
+  	}
+    if (!empty($res)) {
+      return $res[0];
+    } else {
+      return null;
+    }
+  }
 
   function getListaNoticias($sigElem, $elementos) {
     try{
       $sql = "SELECT * FROM Noticia WHERE fecha < now() ORDER BY fecha LIMIT :sigElemento , :numElementos";
-      $stmt->execute(["sigElemento" => $sigElem]);
-      $stmt->execute(["numElementos" => $elementos]);
+      $stmt->execute(["sigElemento" => $sigElem, "numElementos" => $elementos]);
       $stmt = $this->conn->prepare($sql);
       $res = $stmt->fetchAll();
     }
@@ -42,6 +57,32 @@ class DAONoticia {
     } else {
       return null;
     }
+  }
+
+  function insertNoticia($usuario, $titulo, $contenido, $fecha){
+    try{
+      $sql = "INSERT INTO Noticia (usuario, titulo, contenido, fecha) VALUES (:usuario,:titulo, :contenido, :fecha )";
+      $stmt->execute(["usuario" => $usuario, "titulo" => $titulo, "contenido" => $contenido, "fecha" => $fecha]);
+      $stmt->execute();
+      $id = $this->conn->lastInsertId();
+    } catch(PDOException $e) {
+  		echo "ERROR EN DAONoticia: " . $e->getMessage();
+      return null;
+  	}
+    return $id;
+  }
+
+  function updateNoticia($id, $titulo, $contenido) {
+      try {
+        $sql = "INSERT INTO Usuario (id, titulo, contenido) VALUES (:id, :titulo, :contenido)";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->execute(["id" => $id, "titulo" => $titulo, "contenido" => $contenido]);
+        $stmt->execute();
+      } catch(PDOException $e) {
+    		echo "ERROR EN DAONoticia: " . $e->getMessage();
+        return null;
+    	}
+      return true;
   }
 
 }
