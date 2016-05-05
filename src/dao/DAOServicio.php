f17c39a56e9b8b33ea997c1ce3dc92eb60ae7e49
A V <netnoc@gmail.com>
2016-05-05 19:44:36 +0200

    Inicio de DAOServicio

---
+++ src/dao/DAOServicio.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace aw\dao;
+
+class DAOServicio {
+
+  private $conn;
+
+  function __construct() {
+    $this->conn = \aw\dao\Connection::getInstance()->getconnection();
+  }
+
+  function getServicioByCategory($category) {
+      try {
+        $sql = "SELECT * FROM Servicio WHERE categoria = :categoria ORDER BY media_puntuacion DESC";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->execute(["nombre" => $username]);
+        $res = $stmt->fetchAll();
+      } catch(PDOException $e) {
+    		echo "ERROR EN DAOUsuario: " . $e->getMessage();
+    	}
+  }
+
+}
