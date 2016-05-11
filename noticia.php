a3c8ecd4d8801f70406444828f438305172ba280
Yaco <yacosant@ucm.es>
2016-05-11 03:15:06 +0200

    mostrar u ocultar links siguiente y anterior pagina

--- noticia.php
+++ noticia.php
@@ -10,7 +10,7 @@
 	else
 		$ultimaPag = 0;
 
-	$noticias = $logic->buscarNoticias($ultimaPag * $noticiasPorPagina, $noticiasPorPagina);
+$noticias = $logic->buscarNoticias($ultimaPag * $noticiasPorPagina, $noticiasPorPagina);
 
  ?>
 <!DOCTYPE html>
@@ -51,8 +51,27 @@
 
         			</div>
 				</div>
-				<div class="clear"></div>
-				<a href="noticia.php?ultimaPag=<?=$ultimaPag + 1?>">Siguiente página</a>
+				<br>
+			<?php
+
+			$prev=$ultimaPag - 1;
+			if ($ultimaPag!=0) {
+				echo '<a href="noticia.php?ultimaPag='.$prev.'">Página anterior</a>';
+			}
+
+			?>
+
+			<?php
+			$next=$ultimaPag + 1;
+
+			$noticias = $logic->buscarNoticias($next * $noticiasPorPagina, $noticiasPorPagina);
+
+			if (!$noticias==null) {
+				echo "<a href='noticia.php?ultimaPag=" . $next ."'>Página siguiente</a>";
+			}
+
+			?>
+			
 			<?php
 				require('includes/sidebar.php');
 				require('includes/pie.php');
