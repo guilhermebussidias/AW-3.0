2fb8bfc3125b9d7395a0b6a07acbe23e5942aa72
jhersyucm <jhersyno@ucm.es>
2016-05-12 11:48:05 +0200

    Mostrar búsqueda de servicios

--- formBuscar.php
+++ formBuscar.php
@@ -101,10 +101,54 @@
     				}
     				###################
     				#############SERVICIO
-    				if ($servicio){ #hacer un foreach para devolver los servicios
-							foreach ($servicios as $servicio_) {
-								echo $servicio_['id'] ;
-							}
+   				if ($servicio){ #hacer un foreach para devolver los 
+    					$nombreCategoria = $logic->nameCategory($categoria);
+						echo '<h3 class="tituloServicio">' . $nombreCategoria . '</h3>';
+
+						if(!(count($servicios) == 0)){
+			            echo '<table>';
+			                foreach ($servicios as $servicio_){
+			                  	echo '<tr> 
+		                          <td rowspan="4"><img src="' . $servicio_['nombre'] .'" class="imagenServicio" alt="imagen empresa"></td>
+		                          <td class="empresaServicio">' . $servicio_['nombre'] . '</td>
+		                          <td rowspan="2">
+		                              <div class="estrellasMedia">';
+		                              $puntuacion = $servicio_['media_puntuacion'];
+		                              for($i = 0; $i < $puntuacion; $i++){
+		                                echo '<a  data-value="1" title="Votar con 1 estrellas"></a>';
+		                              }                       
+			                    echo '</td> 
+			                        </tr>
+			                        <tr>
+			                          <td class="direccionServicio">' . $servicio_['ubicacion'] . '</td>
+			                        </tr>
+			                        <tr>
+			                          <td class="telefonoServicio">' .  $servicio_['telefono'] . '</td>
+			                          <td>'; 
+			                            if(!is_null(getRole())) {
+			                              echo '<label for="input-categoria-servicio">Puntuación:</label>
+			                                      <select name="categoria-servicio" id="input-categoria-servicio">
+			                                        <option value="1" selected="selected">1</option>
+			                                        <option value="2">2</option>
+			                                        <option value="3">3</option>
+			                                        <option value="4">4</option>
+			                                        <option value="5">5</option>
+			                                        <option value="6">6</option>
+			                                        <option value="7">7</option>
+			                                        <option value="8">8</option>
+			                                        <option value="9">9</option>
+			                                        <option value="10">10</option>
+			                                      </select>';
+			                            } 
+			                     echo '</td>
+			                        </tr>
+			                        <tr>
+			                          <td class="descripcionServicio">' . $servicio_['contenido'] . '</td>
+			                          <td> <a href="'. $servicio_['url'] . '" target= "_blank">'  . $servicio_['url'] . '</a></td>
+			                        </tr>';
+			                 }           
+			          echo' </table>';
+			      	}
     				}
     				##################################
      			?>
