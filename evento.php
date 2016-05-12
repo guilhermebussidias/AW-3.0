bd90cec7b273fe05887e27778d046853966483b0
roberucm <robercnv@gmail.com>
2016-05-12 18:50:16 +0200

    eventos definitiva

--- evento.php
+++ evento.php
@@ -3,7 +3,7 @@
 	$rol = getRole();
 	$id = getId();
 
-  $logic = new \aw\logic\Evento();
+	$logic = new \aw\logic\Evento();
 
 	$eventosPorPagina = 2;
 
@@ -29,44 +29,59 @@
 				require('includes/cabecera.php');
 				require('includes/slider.php');
 			?>
-				<div id="contenido">
-					<div id="contenedor-noticias">
-          <?php
-					foreach($eventos as $evento){
-							echo '
-								<div class="contenido-bloque">
-								<h3 class="contenido-titulo">'. $evento['titulo'] .'</h3>
-								<div class="contenido-info">Escrito por '. $evento['nombre_usuario'] .'</div>';
+			<div id="contenido">
+				<div id="contenedor-evento" class="contenedor">
+				<?php
 
-								if($rol==='admin' or $id===$evento['idUser']){
-									echo '
-									<div class="contenido_admin">
-									<a href="editar-evento.php?evento='. $evento['id'] .'">Editar</a>
-									</div>';
-								}
-								echo '
-								<div class="contenido-texto">
-								<p> ' . $evento['contenido']. ' </p>
-								</div>
-								</div>';
-						}
+				foreach($eventos as $evento){
+         			echo '
+         				<div id="algo" class="contenido-bloque">
+						<h3 class="contenido-titulo">
+						' . $evento['titulo'] . '</a></h3>
+						<div class="contenido-info">Escrito por '. $evento['nombre_usuario'] . ' el ' . $evento['fecha'] .'';
 
-				$pag = $ultimaPag - 1;
-				if($ultimaPag>0){
-					echo "<a href='evento.php?ultimaPag=". $pag ."'>Anterior página</a>";
-				}
-				$sig = $ultimaPag + 1;
-				if (!$eventos==null) {
-					echo "<a href='evento.php?ultimaPag=" . $sig ."'>Página siguiente</a>";
-				}
-				?>
+					if($rol=='admin' or $id==$evento['idUser']){
+						echo '<div class="contenido-admin">
+					<a href="editar-evento.php?evento='. $evento['id'] .'">Editar</a>
+						</div>';
+				 	}
+						echo '</div>
+						<div class="contenido-texto">
+						<p> ' . $evento['contenido'] . ' </p>
+						</div>
+						</div>
+						';
+
+
+    				}
+     			?>
+				<br>
+			<?php
+
+			$prev=$ultimaPag - 1;
+			if ($ultimaPag!=0) {
+				echo '<a href="evento.php?ultimaPag='.$prev.'">Página anterior</a>';
+			}
+
+			?>
+
+			<?php
+			$next=$ultimaPag + 1;
+
+			$eventos = $logic->buscarEventos($next * $eventosPorPagina, $eventosPorPagina);
+
+			if (!$eventos==null) {
+				echo "<a href='evento.php?ultimaPag=" . $next ."'>Página siguiente</a>";
+			}
+
+			?>
 			</div>
-		</div>
+			</div>
+
 			<?php
 				require('includes/sidebar.php');
 				require('includes/pie.php');
 			?>
-
-	</div>
+		</div>
 	</body>
 </html>
