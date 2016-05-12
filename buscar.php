6ed434e481ca1ba586213437479abe58caa5f7a4
A V <netnoc@gmail.com>
2016-05-12 10:36:18 +0200

    Cambios estéticos en búsqueda

--- buscar.php
+++ buscar.php
@@ -30,7 +30,7 @@
 
         ?>
 
-        <div class="maxi-form" >
+        <div class="busqueda-box">
             <div id="contenedor-fragmentos">
               <ul>
                 <li><a href="#busca-noticias">Noticias</a></li>
@@ -40,24 +40,24 @@
 
                 <div id="busca-noticias" class="fragmento-busqueda">
                   <form action="<?= getBasePath() ?>formBuscar.php" method="get" id="form-busqueda-noticia">
-                    <label for="input-titulo-noticia">Título:</label>
-                    <input type="text" name="titulo-noticia" id="input-titulo-noticia" class="input-busqueda-noticia">
-                    <label for="input-contenido-noticia">Contenido:</label>
-                    <input type="text" name="contenido-noticia" id="input-contenido-noticia" class="input-busqueda-noticia">
-                    <label for="input-fecha-ini-noticia">Fecha inicial:</label>
-                    <input type="text" name="fecha-ini-noticia" id="input-fecha-ini-noticia" class="input-busqueda-noticia">
-                    <label for="input-fecha-fin-noticia">Fecha final:</label>
-                    <input type="text" name="fecha-fin-noticia" id="input-fecha-fin-noticia" class="input-busqueda-noticia">
-                 <button type="submit" class="boton-enviar-comentario">Buscar</button>
+                    <label for="input-titulo-noticia" class="label-busqueda">Título:</label>
+                    <input type="text" name="titulo-noticia" id="input-titulo-noticia" class="input-busqueda input-busqueda-noticia">
+                    <label for="input-contenido-noticia" class="label-busqueda">Contenido:</label>
+                    <input type="text" name="contenido-noticia" id="input-contenido-noticia" class="input-busqueda input-busqueda-noticia">
+                    <label for="input-fecha-ini-noticia" class="label-busqueda">Fecha inicial:</label>
+                    <input type="text" name="fecha-ini-noticia" id="input-fecha-ini-noticia" class="input-busqueda input-busqueda-noticia">
+                    <label for="input-fecha-fin-noticia" class="label-busqueda">Fecha final:</label>
+                    <input type="text" name="fecha-fin-noticia" id="input-fecha-fin-noticia" class="input-busqueda input-busqueda-noticia">
+                 <button type="submit" class="boton-enviar-busqueda">Buscar</button>
                  </form>
                 </div>
 
              <div id="busca-servicios" class="fragmento-busqueda">
               <form action="<?= getBasePath() ?>formBuscar.php" method="get"  id="form-busqueda-evento">
                    <p>NO IMPLEMENTADO</p>
-                    <label for="input-contenido-servicio">Contenido:</label>
-                    <input type="text" name="contenido-servicio" id="input-contenido-servicio" class="input-busqueda-evento">
-                    <label for="input-categoria-servicio">Categoría:</label>
+                    <label for="input-contenido-servicio" class="label-busqueda">Contenido:</label>
+                    <input type="text" name="contenido-servicio" id="input-contenido-servicio" class="input-busqueda input-busqueda-evento">
+                    <label for="input-categoria-servicio" class="label-busqueda">Categoría:</label>
                     <select name="categoria-servicio" id="input-categoria-servicio">
                       <option value="veterinario" selected="selected">Veterinarios</option>
                       <option value="residencia">Residencias</option>
@@ -66,32 +66,32 @@
                       <option value="paseador">Paseadores</option>
                       <option value="adopcion">Adopciones</option>
                     </select>
-                    <label for="input-titulo-servicio">Ubicación:</label>
-                    <input type="text" name="ubicacion-servicio" id="input-titulo-servicio" class="input-busqueda-evento">
-                    <label for="input-puntuacion-servicio">Puntuación mínima:</label>
-                    <input readonly type="text" name="puntuacion-servicio" id="input-puntuacion-servicio" class="input-busqueda-evento" value="5">
+                    <label for="input-titulo-servicio" class="label-busqueda">Ubicación:</label>
+                    <input type="text" name="ubicacion-servicio" id="input-titulo-servicio" class="input-busqueda input-busqueda-evento">
+                    <label for="input-puntuacion-servicio" class="label-busqueda">Puntuación mínima:</label>
+                    <input readonly type="text" name="puntuacion-servicio" id="input-puntuacion-servicio" class="input-busqueda input-busqueda-evento" value="5">
                     <div id="slider-puntuacion-servicio"></div>
-               <button type="submit" class="boton-enviar-comentario">Buscar</button>
+               <button type="submit" class="boton-enviar-busqueda">Buscar</button>
               </form>
                </div>
                <div id="busca-eventos" class="fragmento-busqueda">
               <form action="<?= getBasePath() ?>formBuscar.php" method="get" id="form-busqueda-servicio">
                     <p>NO IMPLEMENTADO</p>
-                    <label for="input-titulo-evento">Título:</label>
-                    <input type="text" name="titulo-evento" id="input-titulo-evento" class="input-busqueda-servicio">
-                    <label for="input-contenido-evento">Contenido:</label>
-                    <input type="text" name="contenido-evento" id="input-contenido-evento" class="input-busqueda-servicio">
-                    <label for="input-fecha-ini-evento">Fecha inicial:</label>
-                    <input type="text" name="fecha-ini-evento" id="input-fecha-ini-evento" class="input-busqueda-servicio">
-                    <label for="input-fecha-fin-evento">Fecha final:</label>
-                    <input type="text" name="fecha-fin-evento" id="input-fecha-fin-evento" class="input-busqueda-servicio">
-                    <label for="input-titulo-evento">Ubicación:</label>
-                    <input type="text" name="titulo-evento" id="input-titulo-evento" class="input-busqueda">
-                <button type="submit" class="boton-enviar-comentario">Buscar</button>
+                    <label for="input-titulo-evento" class="label-busqueda">Título:</label>
+                    <input type="text" name="titulo-evento" id="input-titulo-evento" class="input-busqueda input-busqueda-servicio">
+                    <label for="input-contenido-evento" class="label-busqueda">Contenido:</label>
+                    <input type="text" name="contenido-evento" id="input-contenido-evento" class="input-busqueda input-busqueda-servicio">
+                    <label for="input-fecha-ini-evento" class="label-busqueda">Fecha inicial:</label>
+                    <input type="text" name="fecha-ini-evento" id="input-fecha-ini-evento" class="input-busqueda input-busqueda-servicio">
+                    <label for="input-fecha-fin-evento" class="label-busqueda">Fecha final:</label>
+                    <input type="text" name="fecha-fin-evento" id="input-fecha-fin-evento" class="input-busqueda input-busqueda-servicio">
+                    <label for="input-titulo-evento" class="label-busqueda">Ubicación:</label>
+                    <input type="text" name="titulo-evento" id="input-titulo-evento" class="input-busqueda input-busqueda">
+                <button type="submit" class="boton-enviar-busqueda">Buscar</button>
               </form>
              </div>
+						 <div class="clear"></div>
             </div>
-
 			</div>
 			<?php
 				require(getIncludePath() . 'sidebar.php');
