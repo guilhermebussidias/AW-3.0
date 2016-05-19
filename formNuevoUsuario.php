f71a2b5bd35d953d2d2168c9fb45741a36c3f2a1
JoniSanchez <jonisanchezparedes@gmail.com>
2016-05-19 23:15:49 +0200

    ventanita usuario javascript

--- formNuevoUsuario.php
+++ formNuevoUsuario.php
@@ -9,16 +9,18 @@ $role = "normal";
 $logic = new \aw\logic\Usuario();
 $name = trim($name);
 if($name !== '' && $password !== '') {
-	$camposVacios = false;
 	$ok = $logic->newUser($name, $password, $role);
+	if (is_null($ok)){
+		$respuesta = "El usuario ya existe.";
+	}else {
+		$respuesta = "Enhorabuena, ya formas parte de Alldogs";
+	}
 }
 else {
-	$ok = false;
-	$camposVacios = true;
+	$respuesta = "Rellene todos los campos.";
 }
 
 ?>
-
 
 <!DOCTYPE html>
 <html lang="es">
@@ -26,40 +28,12 @@ else {
 		<meta charset="UTF-8">
 		<title>All Dogs</title>
 		<?php require(getIncludePath() . 'head.php'); ?>
+    	<script src="<?= getJSPath() ?>respuesta.js"></script>
 	</head>
 	<body>
-		<div id="contenedor">
-			<?php
-				require(getIncludePath() . 'cabecera.php');
-				require(getIncludePath() . 'slider.php');
-			?>
-			<div id="contenido">
-        <?php
-
-          if ($ok) {
-            echo '
-              <h1>Usuario creado con éxito</h1>
-              <p>Ya puedes hacer login con tu nuevo usuario.</p>
-            ';
-          } else {
-							if (!$camposVacios) {
-								echo '
-		              <h1>Error al crear usuario</h1>
-		              <p>Hubo un error al crear su usuario.
-		              Es posible que ya exista otro usuario con ese nombre.</p>
-		            ';
-							}
-							else {
-								echo '<h1> Los campos no pueden estar vacíos</h1>';
-							}
-          }
-
-        ?>
-			</div>
-			<?php
-				require(getIncludePath() . 'sidebar.php');
-				require(getIncludePath() . 'pie.php');
-			?>
+		<div id="mensaje">
+			<p><?= $respuesta ?></p>
 		</div>
+		<span id="redireccion" style="display:none"><?=  getBasePath() . "index.php" ?></span>
 	</body>
 </html>
